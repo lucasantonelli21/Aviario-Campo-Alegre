aad0899eec0910b0cb4b3d98386ddcb9f9df12ea
Lucas Barbosa Antonelli <lucaslba2105@gmail.com>
2024-08-14 19:34:31 -0300

    criacao-projeto

---
+++ Models/LoteModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aviario_Campo_Alegre.Models
+{
+    public class LoteModel
+    {
+        
+    }
+}
---
+++ Models/RefeicaoModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aviario_Campo_Alegre.Models
+{
+    public class RefeicaoModel
+    {
+        public int Id { get; set; }
+        public int NumeroLote { get; set; }
+        public int IdRacao { get; set; }
+
+
+    }
+}
